d84b1c9d9643c4f27c9a6562ca9d768eed2e17d0
Chase Lowney -Laptop <chasea2016@gmail.com>
2023-01-19 15:56:39 -0500

    first commit, setting everything up

---
+++ finding_words_game.py
@@ -0,0 +1,10 @@
+import random
+
+# accessing the word repository 
+words = []
+with open("word_repository.txt") as word_repo:
+    words = word_repo.read().split()
+
+# getting a list of letters to choose from
+
+# 
